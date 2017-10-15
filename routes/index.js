ab813abccd485a983f8c665d786ec839575d38d8
William Oliveira <w.oliveira542@gmail.com>
2017-10-15 21:41:01 -0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json({ msg: 'Plataforma TrainingCenter' });
+  res.json({ msg: 'Plataforma training-center' });
 });
 
 module.exports = router;
