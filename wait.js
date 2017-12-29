8e497f619ae3c46f6df917ef6ebbaba7bb89cd38
Vitor Capretz <capretzvitor@gmail.com>
2017-12-28 22:19:29 -0200

    chore: add linter to the project
    I used xo for that, which is supposed to run with almost none configs,
    but some changes were necessary to this project
    
    - the indent rule is off because of a eslint bug:
    https://github.com/eslint/eslint/issues/9779

--- wait.js
+++ wait.js
@@ -3,10 +3,10 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 sequelize
- .authenticate()
-    .then(() => {
-      process.exit(0);
-    })
-    .catch(err => {
-      process.exit(1);
-    });
+	.authenticate()
+	.then(() => {
+		console.log('database initiated');
+	})
+	.catch(() => {
+		console.log('database failed to iniciate');
+	});
